c674a838f56ef1938546ad821898d0dff07a63ce
Andrii <scoandrey@gmail.com>
2023-07-26 10:20:34 -0400

    hw3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,12 +18,12 @@ class App extends Component {
   removeContact = id => {
     this.setState(prevState => {
       const contacts = prevState.contacts.filter(cont => cont.id !== id);
+      localStorage.setItem('contacts', JSON.stringify(contacts));
       return { contacts };
     });
   };
 
   addContact = contact => {
-
     const { contacts } = this.state;
     if (
       contacts.find(
@@ -36,6 +36,7 @@ class App extends Component {
     this.setState(prevState => {
       const contacts = [...prevState.contacts];
       contacts.push({ ...contact, id: nanoid() });
+      localStorage.setItem('contacts', JSON.stringify(contacts));
       return { contacts };
     });
   };
